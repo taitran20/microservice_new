d2918b8d39090c765a15800a180ecbfbbfef0c9a
tai <td.tai2000@gmail.com>
2024-03-07 23:14:10 +0700

    create eureka cloud server

--- inventory_service/src/main/java/com/tai/inventory_service/InventoryServiceApplication.java
+++ inventory_service/src/main/java/com/tai/inventory_service/InventoryServiceApplication.java
@@ -1,34 +1,16 @@
 package com.tai.inventory_service;
 
-import com.tai.inventory_service.model.Inventory;
-import com.tai.inventory_service.repository.InventoryRepository;
-import com.tai.inventory_service.service.InventoryService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+//@EnableDiscoveryClient
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
 
-	@Bean
-	public CommandLineRunner loadData(InventoryRepository inventoryRepository){
-		return args -> {
-			Inventory inventory = new Inventory();
-			inventory.setSkuCode("iphone_13");
-			inventory.setQuantity(100);
 
-			Inventory inventory_01 = new Inventory();
-			inventory_01.setSkuCode("iphone_15");
-			inventory_01.setQuantity(0);
-
-			inventoryRepository.save(inventory_01);
-			inventoryRepository.save(inventory);
-		};
-	}
 
 }
